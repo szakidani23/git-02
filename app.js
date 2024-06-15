fbdd6a1384aedd5135624d180702a3bc2af38d7d
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-06-15 09:58:36 +0200

    testString function added

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ function div(a, b) {
   return 'division by zero is not possible!';
 }
 
+let testString = 'jancsika';
+testString.includes('j')
+  ? console.log('yes it contains j')
+  : console.log('no it doesnt contain j');
+
 console.log(add(10, 20));
