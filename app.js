308b02c945a9163211459e972d181a952e163573
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-06-15 10:15:55 +0200

    Comments + multiplication function created

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ let testString = 'jancsika';
 testString.includes('j')
   ? console.log('yes it contains j')
   : console.log('no it doesnt contain j');
-
+// Subtraction function
 function sub(a, b) {
   return a - b;
+}
+// Multiple
+function mul(a, b) {
+  return a * b;
 }
 console.log(add(10, 20));
